9ce6a99decfce4517d8540ee7272394b9c084b12
Nelson Chen <crazysim@gmail.com>
2010-05-28 22:41:46 -0700

    character parsing

--- src/main/java/com/mindflakes/TeamRED/MenuXML/MenuXMLHandler.java
+++ src/main/java/com/mindflakes/TeamRED/MenuXML/MenuXMLHandler.java
@@ -2,6 +2,7 @@ package com.mindflakes.TeamRED.MenuXML;
 
 import java.util.ArrayList;
 
+import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import com.mindflakes.TeamRED.menuClasses.MealMenu;
@@ -16,13 +17,19 @@ import com.mindflakes.TeamRED.menuClasses.MealMenu;
  */
 public class MenuXMLHandler extends DefaultHandler{
 	private ArrayList<MealMenu> menus;
+	private StringBuilder builder;
 	
 	public ArrayList<MealMenu> getMenus(){
 		return this.menus;
 	}
 	
-	
-	
+	@Override
+    public void characters(char[] ch, int start, int length)
+            throws SAXException {
+        super.characters(ch, start, length);
+        builder.append(ch, start, length);
+    }
+
 	
 
 }
