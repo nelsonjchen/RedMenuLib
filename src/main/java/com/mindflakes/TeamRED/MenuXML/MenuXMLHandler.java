09458f767326a4a6727789f442daf1fa7eb1d9fa
Nelson Chen <crazysim@gmail.com>
2010-05-28 22:28:21 -0700

    menus

--- src/main/java/com/mindflakes/TeamRED/MenuXML/MenuXMLHandler.java
+++ src/main/java/com/mindflakes/TeamRED/MenuXML/MenuXMLHandler.java
@@ -1,7 +1,17 @@
 package com.mindflakes.TeamRED.MenuXML;
 
+import java.util.ArrayList;
+
 import org.xml.sax.helpers.DefaultHandler;
 
+import com.mindflakes.TeamRED.menuClasses.MealMenu;
+
+
 public class MenuXMLHandler extends DefaultHandler{
+	private ArrayList<MealMenu> menus;
+	
+	public ArrayList<MealMenu> getMenu(){
+		return this.menus;
+	}
 
 }
