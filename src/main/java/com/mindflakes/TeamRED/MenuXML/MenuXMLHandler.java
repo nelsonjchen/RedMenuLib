220446fae167f0e8c628a8c1178628b3015b4617
Nelson Chen <crazysim@gmail.com>
2010-05-28 22:41:19 -0700

    added documentation and corrected function

--- src/main/java/com/mindflakes/TeamRED/MenuXML/MenuXMLHandler.java
+++ src/main/java/com/mindflakes/TeamRED/MenuXML/MenuXMLHandler.java
@@ -7,11 +7,22 @@ import org.xml.sax.helpers.DefaultHandler;
 import com.mindflakes.TeamRED.menuClasses.MealMenu;
 
 
+/**
+ * Based off of example as seen at
+ * <a href=http://tinyurl.com/ykvykfp>this article</a>.
+ *
+ * @author Nelson Chen
+ * 
+ */
 public class MenuXMLHandler extends DefaultHandler{
 	private ArrayList<MealMenu> menus;
 	
-	public ArrayList<MealMenu> getMenu(){
+	public ArrayList<MealMenu> getMenus(){
 		return this.menus;
 	}
+	
+	
+	
+	
 
 }
